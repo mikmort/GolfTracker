804f3c63795e906d58ac9ed47193c768f949a5e7
Mike Morton <mikmort@hotmail.com>
2025-07-12 00:38:34 +0200

    Change target to MacOS 10.15+

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "GolfTracker",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
+        .macOS(.v10_15)
     ],
     targets: [
         .executableTarget(
